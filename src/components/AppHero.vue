d4008b03fa3412b14ff399a3e47e42e4bee8537f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-09 14:37:01 +0200

    [add]:component AppHero jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: 'AppHero',
+  data(){
+    return {
+      titleJumbo: 'Jumbotron'
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="jumbotron">
+    <h2>{{ titleJumbo }}</h2>
+    <img src="../../public/vite.svg" alt="vite">
+  </div>
+</template>
+
+
+<style scoped>
+  #jumbotron {
+    margin-top: 50px;
+    text-align: center;
+    color: rgb(52, 71, 95);
+  }
+
+  h2 {
+    font-size: 1.7rem;
+  }
+
+  img {
+    margin-top: 20px;
+    width: 100px;
+  }
+
+
+
+</style>
